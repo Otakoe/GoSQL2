bddb472402c598c3d033e05d795f61b0e34ba99e
Otakoe <vitalylarc@gmail.com>
2020-04-20 16:50:06 +0300

    Выбор конкретного проекта по айди

--- 6.sql
+++ 6.sql
@@ -4,9 +4,6 @@ select pr.project_name as cheaper_project,avg(dev.salary)
 	on ldp.dev_id=dev.id
     join projects pr
 	on pr.id = ldp.project_id
-    where pr.project_name=
-	(select project_name
-		from projects
-		where cost = (select min(cost) from projects))
-    group by pr.project_name;
+    where pr.id = (select id from projects 
+					where cost = (select min(cost) from projects));
         
